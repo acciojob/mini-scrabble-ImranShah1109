0448a2b1f3935918833d107762ff4d862a33ad90
ImranShah1109 <imranshaha41@gmail.com>
2023-01-24 17:06:41 +0530

    update in script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+
+let text = document.getElementById("evaluatedText");
+    let heading = document.getElementById("letterCount");
+    text.addEventListener('input',countLetter);
+    
+
+
+    function countLetter(){
+      let word = text.value;
+      heading.innerHTML = word.length;
+
+    }
